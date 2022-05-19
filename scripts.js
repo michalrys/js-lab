295e3ef8206b2ebca57141678eb6cef0ead7c751
michalrys <michalrys@gmail.com>
2022-05-19 20:59:16 +0200

    show / hide button

--- scripts.js
+++ scripts.js
@@ -14,12 +14,18 @@ function readValues() {
     }
 }
 
-function runStart() {
-    let buttonStop = document.querySelector("button.stop");
-    let buttonStart = document.querySelector("button.start");
-    buttonStart.style.background = "rgb(154,45,217)";
-    buttonStart.style.display = "none";
-    buttonStop.style.display = "";
+function runningButtonClicked() {
+    let button = document.querySelector("button.runningButton");
+    var currentText = button.innerText;
+    if(currentText === "START") {
+        button.innerText = "STOP";
+        button.style.background = "rgb(255, 0, 0)";
+        button.style.borderColor = "rgb(255, 0, 0)";
+    } else {
+        button.innerText = "START";
+        button.style.background = "rgb(66, 255, 12)";
+        button.style.borderColor = "rgb(66, 255, 12)";
+    }
 }
 function runStop() {
     let buttonStop = document.querySelector("button.stop");
